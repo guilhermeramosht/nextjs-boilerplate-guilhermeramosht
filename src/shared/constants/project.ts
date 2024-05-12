748fa45de55bd606d34c1b180d4194904d64f3e5
guilhermeramosht <guilhermeht.ramos@gmail.com>
2024-05-12 00:50:06 -0300

    feat(config): creating project json to store project info

---
+++ src/shared/constants/project.ts
@@ -0,0 +1,12 @@
+const project = {
+  appName: 'Theo Framework',
+  navItems: [
+    { key: 'home', href: '/' },
+    { key: 'aboutUs', href: '/about-us' },
+    { key: 'services', href: '/services' },
+    { key: 'portfolio', href: '/portfolio' },
+    { key: 'contact', href: '/contact' }
+  ]
+}
+
+export default project
